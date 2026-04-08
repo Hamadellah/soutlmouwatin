5ef56c40c823014cec464b43e504aa5565804221
Hamadellah <hamadellahotman13@gmail.com>
2026-04-08 15:26:36 +0100

    Refactor App and main components for consistency; streamline imports and remove unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import SmartCityMorocco from './SmartCityMorocco'
+import SmartCityMorocco from "./SmartCityMorocco";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
- <SmartCityMorocco/>
-    </>
-  )
+  return <SmartCityMorocco />;
 }
 
-export default App
+export default App;
